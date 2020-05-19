72ee6fff4160d2cfe19e896b945b50d4f32e3436
Matt Clegg <m@cle.gg>
2020-05-19 07:44:06 +0545

    debugging

--- app/src/PageController.php
+++ app/src/PageController.php
@@ -14,29 +14,10 @@ namespace {
 		];
 
 		public function pdf() {
-			$stylesheet = 'vendor/grasenhiller/silverstripe-intranet-wiki/client/css/pdf.css';
-
-			$filter = URLSegmentFilter::create();
-			$filename = $filter->filter($this->MenuTitle);
-			$filename .= '__' . date('Y-m-d') . '.pdf';
-
-			$baseUrl = Director::absoluteBaseURL();
-
-			$pdf = new WkPdf();
-			$pdf->setOption('margin-top', 20);
-			$pdf->setOption('margin-bottom', 20);
-			$pdf->setOption('margin-left', 15);
-			$pdf->setOption('margin-right', 15);
-			$pdf->setOption('user-style-sheet', $stylesheet);
-			$pdf->setOption('header-html', $baseUrl . 'gh-wkhtmltox/header?template=Grasenhiller\Intranet\Wiki\Pages\Pdf\Header');
-			$pdf->setOption('footer-html', $baseUrl . 'gh-wkhtmltox/footer?template=Grasenhiller\Intranet\Wiki\Pages\Pdf\Footer');
-			$pdf->setOption('header-spacing', 5);
-			$pdf->setOption('footer-spacing', 5);
-
-			$html = $pdf::get_html($this, [], '', 'pdf');
-
-			$pdf->add($html);
-			$pdf->download($filename);
+			$pdf = new SS_DOMPDF();
+			$pdf->setHTML($this->renderWith('Pdf'));
+			$pdf->render();
+			$pdf->toFile($this->Link());
 		}
 	}
 }
